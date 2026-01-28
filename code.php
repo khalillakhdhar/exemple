bd524986fc263cb7682152a68173e3f65b293f74
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2026-01-28 14:18:58 +0100

    ajout

--- code.php
+++ code.php
@@ -1,3 +1,10 @@
 <?php
 require("db.php");
 ?>
+# add user to table user(id,nom,prenom,email,password,age)
+<?php
+#query
+$sql = "INSERT INTO `users`(`nom`, `prenom`, `age`, `login`, `password`) VALUES ('Doe', 'John', 30, 'john.doe@example.com', 'password123')";
+$conn->exec($sql);
+echo "New record created successfully";
+?>

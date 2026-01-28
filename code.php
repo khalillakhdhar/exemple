44c14e54ec110f1c5acc2fa4a6dfcc515a3207a4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2026-01-28 14:20:28 +0100

    Update code.php

--- code.php
+++ code.php
@@ -1,7 +1,6 @@
 <?php
 require("db.php");
 ?>
-# add user to table user(id,nom,prenom,email,password,age)
 <?php
 #query
 $sql = "INSERT INTO `users`(`nom`, `prenom`, `age`, `login`, `password`) VALUES ('Doe', 'John', 30, 'john.doe@example.com', 'password123')";
